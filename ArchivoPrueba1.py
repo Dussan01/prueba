8a6eba82d01811c7c6bca99cccd5b1c79d8cbd94
Dussan01 <Dussan01@users.noreply.github.com>
2020-12-21 17:04:20 -0500

    Pruebas modificaciones en el desarrollo pricnicpal

--- ArchivoPrueba1.py
+++ ArchivoPrueba1.py
@@ -1,3 +1,9 @@
 print("Hello Work!")
 print("Hello Work!")
 print("Prueba Work!")
+
+
+
+# Es mejor de esta foma a ver como funciona si hay alteraciones en otros lados de los proeyeto
+print("Prueba Work!")
+
